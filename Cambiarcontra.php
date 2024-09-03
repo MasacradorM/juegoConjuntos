18d5a84c17901935b704d89bdd7f633149f72f05
Andress1117 <juanfelipe11172005@gmail.com>
2024-09-03 11:13:21 -0500

    trabajo

---
+++ Cambiarcontra.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Recuperación de Contraseña</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="form-wrapper">
+            <h2>Recuperación de Contraseña</h2>
+            <form>
+                <label for="nueva_contrasena">Nueva contraseña:</label>
+                <input type="password" id="nueva_contrasena" name="nueva_contrasena" required>
+                <label for="confirmar_contrasena">Confirmar contraseña:</label>
+                <input type="password" id="confirmar_contrasena" name="confirmar_contrasena" required>
+                <button type="submit">Siguiente</button>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
---
+++ codigo.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Iniciar sesión</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="css/style.css">  
+</head>
+<body>
+    <div class="container">
+        <div class="form-wrapper form-wrapperr">
+            <h2>Recuperación de Contraseña</h2>
+            <p>Ingresa el código enviado a tu correo</p>
+            <form>
+                <input type="text" id="codigo" placeholder="Código" min="0">
+                <button class="boton1" type="submit">Siguiente</button>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
